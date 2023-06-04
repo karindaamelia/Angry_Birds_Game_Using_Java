3aa00470c38ca5bfa78d4ffa422ef9d57edcd9c3
Karinda Amelia <74167008+karindaamelia@users.noreply.github.com>
2023-06-04 13:58:27 +0700

    Upload Skills.java

---
+++ Skills.java
@@ -0,0 +1,8 @@
+package AngryBird;
+
+public interface Skills {
+	public int birdSkill();
+	public void displaySkill();
+	public int attack(int enemyDef);
+	public void displayAttack(int enemyDef);
+}
